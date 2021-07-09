40bdbda358cf457234bd9c827a96bfd29bc78c06
erripollo <pollone.enrico@gmail.com>
2021-07-09 07:02:13 +0200

    Create links db in config directory

---
+++ config/links.php
@@ -0,0 +1,40 @@
+<?php 
+return [
+    'dc comics' => [
+        'Characters',
+        'Comics',
+        'Movies',
+        'Tv',
+        'Games',
+        'Videos',
+        'News'
+    ],
+    'shop' => [
+        'Shop DC',
+        'Shop DC Collectibles'
+    ],
+    'dc' => [
+        'Terms of use',
+        'Privacy policy (New)',
+        'Ad choices',
+        'Advertising',
+        'Jobs',
+        'Subscriptions',
+        'Talent workshops',
+        'CPSC certificates',
+        'Ratings',
+        'Shop help',
+        'Contact Us'
+    ],
+    'sites' => [
+        'DC',
+        'MAD Magazine',
+        'DC Kids',
+        'DC Universe',
+        'DC Power Visa'
+    ]
+];
+
+
+
+?>
