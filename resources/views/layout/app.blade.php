5b606cf666e7f79ba6276ecdcdab547f4fc220de
erripollo <pollone.enrico@gmail.com>
2021-07-09 08:30:36 +0200

    complete footer bottom

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -89,6 +89,25 @@
                 </div>
 
             </div>
+
+        </div>
+        
+        <div class="footer_bottom">
+            <div class="container">
+                <div class="btn">
+                    <span class="btn_outline">
+                        Sign-up now!
+                    </span>
+                </div>
+                <div class="social">
+                    <span>Follow us</span>
+                    <img src="./img/footer-facebook.png" alt="">
+                    <img src="./img/footer-twitter.png" alt="">
+                    <img src="./img/footer-youtube.png" alt="">
+                    <img src="./img/footer-pinterest.png" alt="">
+                    <img src="./img/footer-periscope.png" alt="">
+                </div>
+            </div>
         </div>
 
     </footer>
