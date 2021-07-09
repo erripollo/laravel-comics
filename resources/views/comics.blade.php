4f79a6c419cc7f0718fbc9ec18c210bd744620e4
erripollo <pollone.enrico@gmail.com>
2021-07-09 07:01:52 +0200

    Complete main section in comics view

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,16 +3,58 @@
 @section('title', 'Comics | DC')
     
 @section('content')
-    <div class="cards container">
-        @foreach ($comics as $comic)
-        <div class="card">
-            <img src="{{$comic['thumb']}}" alt="">
-            <div class="title">
-                {{$comic['series']}}
+
+    <div class="container">
+        <div class="label">
+            Current series
+        </div>
+        <div class="cards">
+            @foreach ($comics as $comic)
+            <div class="card">
+                <img src="{{$comic['thumb']}}" alt="">
+                <div class="title">
+                    {{$comic['series']}}
+                </div>
+            </div>
+            @endforeach    
+        </div>
+        
+        <div class="btn">
+            <span class="btn_solid">Load more</span>
+        </div>
+    </div>
+    <div class="main_bottom">
+        <div class="container">
+            <div class="card">
+                <img src="./img/buy-comics-digital-comics.png" alt="">
+                <div>
+                    Digital comics
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/buy-comics-merchandise.png" alt="">
+                <div>
+                    Dc merchandise
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/buy-comics-subscriptions.png" alt="">
+                <div>
+                    Subscriptions
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/buy-comics-shop-locator.png" alt="">
+                <div>
+                    Comic shop locator
+                </div>
+            </div>
+            <div class="card">
+                <img src="./img/buy-dc-power-visa.svg" alt="">
+                <div>
+                    Dc power visa
+                </div>
             </div>
         </div>
-            
-        @endforeach
-        
     </div>
 @endsection
