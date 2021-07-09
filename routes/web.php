5b3694ee348c91958963d5a2fd0e7f8090663d71
erripollo <pollone.enrico@gmail.com>
2021-07-09 15:54:16 +0200

    implement click function on comics cards

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,18 @@ Route::get('/comics', function () {
     return view('comics.index', compact('comics'));
 })->name('comics');
 
-Route::get('comics/(id)', function ($id){
-
+Route::get('comics/{id}', function ($id){
     $comics = config('comics');
     //dd($comics[$id]);
-    $comic = $comics[$id];
 
-    return view('comics.show', compact('comic'));
-
-});
+    if (is_numeric($id) && $id < count($comics) && $id >= 0){
+        $comic = $comics[$id];
+        return view('comics.show', compact('comic'));
+        
+    }else{
+        abort(404);
+    }
+})->name('comic');
 
 
 
