402388791410ab79c33eeb9ee69b6fd363a6fe95
erripollo <pollone.enrico@gmail.com>
2021-07-09 16:35:33 +0200

    create initial strucute html comic detail page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,6 +4,31 @@
     
 @section('comic_content')
 
-    <h1>{{$comic['title']}}</h1>
+    <div class="comic_main">
+        <div class="banner">
+    
+        </div>
+
+        <div class="container">
+            <div class="info">
+                <h2>{{$comic['title']}}</h2>
+
+                <div class="price_table">
+                    <span>U.S. Price: {{$comic['price']}}</span>
+                    <span>Check Availability</span>
+                </div>
+
+                <div class="desc">
+                    <p>
+                        {{$comic['description']}}
+                    </p>
+                </div>
+            </div>
+            
+            <div class="adv">
+                <img src="{{asset('img/adv.jpg')}}" alt="">
+            </div>
+        </div>
+    </div>
    
 @endsection
