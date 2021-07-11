d0b29d4f6799eca57a7193070dd395a5a545ddfb
erripollo <pollone.enrico@gmail.com>
2021-07-11 14:55:11 +0200

    Complete talent and specs section in comic detail

--- resources/views/comics/partials/bottom_section.blade.php
+++ resources/views/comics/partials/bottom_section.blade.php
@@ -3,14 +3,17 @@
 <div class="bottom_section">
     <div class="container">
 
-        <div class="col_right">
+        <div class="col_left">
             <h3>Talent</h3>
 
             <div class="info">
                 <div class="info_title">Art by:</div>
                 <div class="info_section">
                     @foreach ($comic['artists'] as $artist)
-                        <span>{{$artist}}, </span>
+                        <a href="#">{{$artist}}, </a>
+                        @if ($loop->last)
+                            <a href="#">{{$artist}}</a>
+                        @endif
                     @endforeach
                 </div>
             </div>
@@ -18,17 +21,22 @@
                 <div class="info_title">Written by:</div>
                 <div class="info_section">
                     @foreach ($comic['writers'] as $writer)
-                        <span>{{$writer}}, </span>
+                        <a href="#">{{$writer}}, </a>
+                        @if ($loop->last)
+                            <a href="#">{{$writer}}</a>
+                        @endif
                     @endforeach
                 </div>
             </div>
         </div>
-        <div class="col_left">
+        <div class="col_right">
             <h3>Specs</h3>
             <div class="info">
                 <div class="info_title">Series:</div>
                 <div class="info_section">
-                   {{$comic['series']}}
+                    <a href="#">
+                        {{$comic['series']}}
+                    </a>
                 </div>
             </div>
             <div class="info">
